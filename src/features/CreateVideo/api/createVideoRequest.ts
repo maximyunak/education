2e759119446c2315262c4093cb9204f3715e61a1
Maxik-web <kleok0039@gmail.com>
2025-02-04 11:33:16 +0300

    fix

--- src/features/CreateVideo/api/createVideoRequest.ts
+++ src/features/CreateVideo/api/createVideoRequest.ts
@@ -1,9 +1,4 @@
 import $api from 'shared/api/baseUrl';
 import ls from 'localstorage-slim';
 
-interface AuthResponse {
-  access_token: string;
-  token_type: string;
-}
-
-export const authRequest = async (userData: LoginDataType): Promise<void> => {};
+export const authRequest = async (): Promise<void> => {};
